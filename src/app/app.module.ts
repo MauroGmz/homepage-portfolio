f289fcbb58f51daca0ab5c0db562c5cb9efeec86
MauroGmz <mauro.f.gmz@gmail.com>
2021-01-23 03:50:46 -0300

    added myProjects component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './principal/principal.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrincipalComponent
+    PrincipalComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/projects/projects.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.scss']
+})
+export class ProjectsComponent implements OnInit {
+
+  constructor(private title: Title) { }
+
+  ngOnInit() {
+    this.title.setTitle('My Projects - MauroGmz');
+  }
+
+}
